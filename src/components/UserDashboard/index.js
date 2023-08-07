e66a240a255450901604588b30c47c805dcc7733
sri vidya <86715568+sri-vidya09@users.noreply.github.com>
2023-08-07 08:04:21 +0530

    login

--- src/components/UserDashboard/index.js
+++ src/components/UserDashboard/index.js
@@ -20,7 +20,7 @@ const apiStatusConstants = {
 
 
 const UserDashboard = () => {
-    const [cookie, _] = useCookies(["user_id"])
+    {renderTransactionProfile(transaction.user_id)}
     const [transactions, setTransaction] = useState([])
     const [apiStatus, setApiStatus] = useState(apiStatusConstants.initial)
     const [apiStatusOne, setApiStatusOne] = useState(apiStatusConstants.initial)
